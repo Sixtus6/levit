0eb818ab4e7dbb83b9d6a037fcf4a049649d7616
Sixtus <6ixtus6@gmail.com>
2024-04-30 22:00:50 +0100

    added the images config

---
+++ lib/config/images.dart
@@ -0,0 +1,7 @@
+class ImageConfig {
+  static const String image1 = "assets/images/image1.png";
+  static const String image2 = "assets/images/image2.png";
+  static const String image3 = "assets/images/image3.png";
+  static const String image4 = "assets/images/image4.png";
+  static const String image5 = "assets/images/image4.png";
+}
