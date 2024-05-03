fc1be332f784d2e573bcac9bca2361bf13151bca
Sixtus <6ixtus6@gmail.com>
2024-05-03 19:37:21 +0100

    created custom app bar

--- lib/views/homepage/home_view.dart
+++ lib/views/homepage/home_view.dart
@@ -401,64 +401,8 @@ class HomeScreenView extends StatelessWidget {
                               )
                             ],
                           ),
-                    bottomNavigationBar: customNavBar()),
+                    bottomNavigationBar: const customNavBar()),
               ),
             )));
-  }
-}
-
-class customAppbar extends StatelessWidget {
-  const customAppbar({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Image.asset(
-          ImageConfig.map2,
-          height: 35,
-        ),
-        const Text(
-          "매일 새로운 친구들을 소개시켜드려요",
-          style: TextStyle(
-              color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),
-        ),
-        Container().expand(),
-        Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: ColorConfig.shade),
-            color: Colors.black,
-
-            borderRadius: BorderRadius.circular(100),
-            // color: Colors.black,
-          ),
-          height: 35,
-          width: 100,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Image.asset(
-                ImageConfig.starIcon,
-                color: ColorConfig.primary,
-                height: 18,
-              ),
-              Text(
-                "323,333",
-                style: TextStyle(
-                  color: Color(0xffF5F5F5),
-                  fontSize: 14,
-                ),
-              ),
-            ],
-          ),
-        ),
-        Image.asset(
-          ImageConfig.bell,
-          height: 42,
-        ),
-      ],
-    );
   }
 }
