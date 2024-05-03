9e804012d7dc123587f78f990ba7e486498ef452
Sixtus <6ixtus6@gmail.com>
2024-05-03 01:42:45 +0100

    cleamed up code  for card 3

---
+++ lib/widget/cards_text.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:luvit/config/color.dart';
+import 'package:luvit/config/images.dart';
+import 'package:luvit/views/homepage/home_view.dart';
+import 'package:nb_utils/nb_utils.dart';
+
+class card2 extends StatelessWidget {
+  const card2({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(60),
+                color: Colors.black,
+              ),
+              height: 35,
+              width: 90,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    ImageConfig.starIcon,
+                    height: 20,
+                  ),
+                  Text(
+                    "29,930",
+                    style: TextStyle(color: ColorConfig.white, fontSize: 15),
+                  ),
+                ],
+              ),
+            ).paddingBottom(10),
+            Row(
+              children: [
+                Text(
+                  "잭과분홍콩나물",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: ColorConfig.white,
+                      fontSize: 28),
+                ),
+                Text(
+                  " 25",
+                  style: TextStyle(color: ColorConfig.gray, fontSize: 25),
+                ),
+              ],
+            ),
+            Text(
+              "서로 아껴주고 힘이 되어줄 사람 찾아요",
+              style: TextStyle(color: ColorConfig.gray, fontSize: 15),
+            ),
+            5.height,
+            Text(
+              "선릉으로 직장 다니고 있고 여행 좋아해요",
+              style: TextStyle(color: ColorConfig.gray, fontSize: 15),
+            ),
+            5.height,
+            Text(
+              "이상한 이야기하시는 분 바로 차단입니다",
+              style: TextStyle(color: ColorConfig.gray, fontSize: 15),
+            ),
+          ],
+        ),
+        Container().expand(),
+        Image.asset(
+          ImageConfig.loveCircle,
+          height: 50,
+        ).paddingTop(100)
+      ],
+    );
+  }
+}
+
+class card3 extends StatelessWidget {
+  const card3({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(60),
+                color: Colors.black,
+              ),
+              height: 35,
+              width: 90,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    ImageConfig.starIcon,
+                    height: 20,
+                  ),
+                  Text(
+                    "29,930",
+                    style: TextStyle(color: ColorConfig.white, fontSize: 15),
+                  ),
+                ],
+              ),
+            ).paddingBottom(10),
+            Row(
+              children: [
+                Text(
+                  "잭과분홍콩나물",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: ColorConfig.white,
+                      fontSize: 28),
+                ),
+                Text(
+                  " 25",
+                  style: TextStyle(color: ColorConfig.gray, fontSize: 25),
+                ),
+              ],
+            ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                border: Border.all(color: Color(0xffFF006B)),
+                color: Color(0xff621133).withOpacity(0.6),
+              ),
+              height: 45,
+              width: 170,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Image.asset(
+                    ImageConfig.lovePink,
+                    height: 20,
+                  ),
+                  const Text(
+                    "진지한 연애를 찾는 중",
+                    style: TextStyle(
+                        color: Color(0xffFF006B),
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ).paddingBottom(7),
+            Row(
+              children: [
+                const itemWidget(
+                  img: ImageConfig.wine,
+                  width: 100,
+                  text: '전혀 안 함',
+                ).paddingBottom(4.5),
+                7.width,
+                itemWidget(
+                  img: ImageConfig.ciga,
+                  width: 80,
+                  text: '비흡연',
+                ).paddingBottom(4.5),
+              ],
+            ),
+            itemWidget(
+              img: ImageConfig.muscle,
+              width: 140,
+              text: '매일 1시간 이상',
+            ).paddingBottom(4.5),
+            Row(
+              children: [
+                itemWidget(
+                  img: ImageConfig.handIcon,
+                  text: '만나는 걸 좋아함',
+                  width: 140,
+                ).paddingBottom(4.5),
+                7.width,
+                itemWidget(
+                  img: '',
+                  width: 60,
+                  text: 'INFB',
+                ).paddingBottom(4.5),
+              ],
+            ),
+          ],
+        ),
+        Container().expand(),
+        Image.asset(
+          ImageConfig.loveCircle,
+          height: 50,
+        ).paddingTop(200)
+      ],
+    );
+  }
+}
---
+++ lib/widget/item_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class itemWidget extends StatelessWidget {
+  const itemWidget({
+    super.key,
+    required this.width,
+    required this.img,
+    required this.text,
+  });
+  final double width;
+  final String img;
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        color: Colors.black,
+      ),
+      height: 40,
+      width: width,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: img == ''
+            ? [
+                Text(
+                  text,
+                  style: TextStyle(
+                    color: Color(0xffF5F5F5),
+                    fontSize: 14,
+                  ),
+                ),
+              ]
+            : [
+                Image.asset(
+                  img,
+                  height: 18,
+                ),
+                Text(
+                  text,
+                  style: TextStyle(
+                    color: Color(0xffF5F5F5),
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+      ),
+    );
+  }
+}
